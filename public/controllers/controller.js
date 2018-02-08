e5d4bcb8267c53e4688c16ac09ff5dd01977c0d2
coreykadd <coreymangan22@live.ie>
2018-02-08 20:30:53 +0000

    added test data in table

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -7,5 +7,27 @@ myApp.controller('AppCtrl',[
     '$http',
     function($scope, $http){
         console.log('Hello World');
+
+        person1 = {
+            name: 'Tim',
+            email: 'email@email.com',
+            number: '0851290182'
+        };
+
+        person2 = {
+            name: 'John',
+            email: 'random@email.ie',
+            number: '0872290193'
+        };
+
+        person3 = {
+            name: 'Sarah',
+            email: 'yeah@email.com',
+            number: '0891044791'
+        };
+
+        var contactList = [person1, person2, person3];
+
+        $scope.contactList = contactList;
     }
 ]);
