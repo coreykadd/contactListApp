0025e07db5a2988973a8e071e5a3493eceb4df22
coreykadd <coreymangan22@live.ie>
2018-02-08 17:10:42 +0000

    setup html and angular

---
+++ public/controllers/controller.js
@@ -0,0 +1,11 @@
+//controller.js
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('AppCtrl',[
+    //'$scope',
+    //'$http',
+    function($scope, /*$http*/){
+        console.log('Hello World');
+    }
+]);
--- server.js
+++ server.js
@@ -3,9 +3,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
     res.send('Hello World');
-});
+});*/
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, function(){
     console.log('App listening on port 8080');
