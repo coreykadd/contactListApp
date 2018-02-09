855b1b00a2859fc42a6e1f67395e49d77d6cf419
coreykadd <coreymangan22@live.ie>
2018-02-09 11:36:09 +0000

    Getting data entered and refreshing page

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -8,14 +8,24 @@ myApp.controller('AppCtrl',[
     function($scope, $http){
         console.log('Hello World');
 
-        $http.get('/contactList')
-        .then(function(res){
-            console.log('Recieved data');
-            $scope.contactList = res.data;
-        });
+        var refresh = function() {
+            $http.get('/contactList')
+            .then(function(res){
+                console.log('Recieved data');
+                $scope.contactList = res.data;
+                $scope.contact = {};
+            });
+        };
+
+        refresh();
 
         $scope.addContact = function(){
             console.log($scope.contact);
+            $http.post('/contactList', $scope.contact)
+            .then(function(res){
+                console.log(res);
+                refresh();
+            });
         };
     }
 ]);
--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var contactList = require('./public/models/contactList.model.js');
+var bodyParser = require('body-parser');
 
 /*app.get('/', function(req, res){
     res.send('Hello World');
 });*/
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/contactList');
 
@@ -28,10 +30,26 @@ app.get('/contactList', function(req, res){
     contactList.find(function(err, data){
         if(err)
             res.send(err);
-            
+
         console.log(data);
         res.json(data);
     });  
+});
+
+app.post('/contactList', function(req, res){
+    console.log(req.body);
+
+    var contact = new contactList();
+    contact.name = req.body.name;
+    contact.email = req.body.email;
+    contact.number = req.body.number;
+
+    contact.save(function(err){
+        if(err)
+            res.send(err);
+
+        res.json({message: 'Contact Added'});
+    });
 });
 
 app.listen(8080, function(){ 
